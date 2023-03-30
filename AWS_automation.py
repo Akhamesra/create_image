10da3625eb405101f49a4d3469b2fd6e61bfabf5
Akhamesra <akhamesra31@gmail.com>
2023-03-30 16:22:06 +0530

    Create backup using ip

--- AWS_automation.py
+++ AWS_automation.py
@@ -29,35 +29,42 @@ def check_ref_id(refid):
     return False
 
 @ami_cli.command('create_ami')
-@click.option('-n','--ec2_name', prompt = 'Enter Name of EC2 instance')
-@click.option('-i', '--image_name', prompt = 'Enter Name to give to AMI')
 @click.option('-r', '--refid', prompt = 'Enter REF_ID')
-def create_ami(ec2_name,image_name,refid):
+@click.option('-n','--ec2_name')
+@click.option('-i', '--ec2_ip')
+def create_ami(refid,ec2_name=None,ec2_ip=None):
   # image_name = input('Enter Name to give to AMI : ')
   # refid = input('Enter REF_ID : ')
   # image_name =  os.getenv("Image_name")
   # refid = os.getenv("REF_ID")
-
   if not check_ref_id(refid):
     print('REF_ID format is incorrect')
     return
   
   # mod_key = 'tag:'+'key'
-  filters =[{'Name':'tag:Name','Values':[ec2_name]}]
+  if ec2_name:
+    filters =[{'Name':'tag:Name','Values':[ec2_name]}]
+
+  elif ec2_ip:
+    filters = [{'Name': 'network-interface.addresses.private-ip-address', 'Values': [ec2_ip]}]
+
   instances = ec2_resource.instances.filter(Filters=filters)
 
   for instance in instances:
+    for tag in instance.tags:
+        if tag['Key'] == 'Name':
+          instance_name = tag['Value']
     instance_id = instance.id
   
+  image_name = instance_name+'-BACKUP'
   date = datetime.datetime.now().strftime("%Y-%m-%d, %H:%M")
-  
   response = ec2_client.create_image(InstanceId=instance_id,
                                      Name = image_name,
                                      NoReboot =  True,
                                      TagSpecifications=[{
                                           'ResourceType' : 'image',
                                           'Tags': [ {'Key': 'Name',         'Value': image_name},
-                                                    {'Key': 'Instance_name','Value': ec2_name},
+                                                    {'Key': 'Instance_name','Value': instance_name},
                                                     {'Key': 'Date',         'Value': str(date)},                                              
                                                     {'Key': 'REF_ID',       'Value': refid},
                                                   ]
